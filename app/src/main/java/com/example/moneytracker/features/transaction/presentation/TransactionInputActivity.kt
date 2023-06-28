4785c789880444b7c3c85d8254db44197d1160d1
Suca <sucanabo@gmail.com>
2023-06-28 16:43:50 +0700

    simple validate + focus first error

--- app/src/main/java/com/example/moneytracker/features/transaction/presentation/TransactionInputActivity.kt
+++ app/src/main/java/com/example/moneytracker/features/transaction/presentation/TransactionInputActivity.kt
@@ -23,6 +23,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import studio.carbonylgroup.textfieldboxes.ExtendedEditText
+import studio.carbonylgroup.textfieldboxes.TextFieldBoxes
 
 class TransactionInputActivity : AppCompatActivity(), CoroutineScope {
 
@@ -34,31 +35,45 @@ class TransactionInputActivity : AppCompatActivity(), CoroutineScope {
         MoneyTrackerRepository.create(db = MoneyTrackerDb(applicationContext))
     }
 
+    lateinit var btnDelete: Button
+    lateinit var btnSave: Button
+    lateinit var swTran: StickySwitch
+
+    lateinit var tfAmount: ExtendedEditText
+    lateinit var tfName: ExtendedEditText
+    lateinit var tfDate: ExtendedEditText
+    lateinit var tfNote: ExtendedEditText
+
+    var isAllFieldValidate: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_transaction)
 
         var tranType: TransactionType = TransactionType.EXPENSE
-        val tAmount = findViewById<ExtendedEditText>(R.id.tfe_tran_amount)
-        val tTitle = findViewById<ExtendedEditText>(R.id.tfe_tran_title)
-        val tDate = findViewById<ExtendedEditText>(R.id.tfe_tran_date)
-        val tNote = findViewById<ExtendedEditText>(R.id.tfe_tran_note)
+        swTran = findViewById(R.id.sw_tran)
+        tfAmount = findViewById(R.id.tfe_tran_amount)
+        tfName = findViewById(R.id.tfe_tran_title)
+        tfDate = findViewById(R.id.tfe_tran_date)
+        tfNote = findViewById(R.id.tfe_tran_note)
+        btnSave = findViewById(R.id.btn_tran_save)
+        btnDelete = findViewById(R.id.btn_tran_delete)
 
         val initData = intent.getSerializableExtra("model") as TransactionModel?
         initData.let { model ->
             if (model != null) {
-                title = "Edit Transaction"
                 tranType = model.type
-                tAmount.setText(model.money.toString())
-                tTitle.setText(model.title)
-                tDate.setText(model.date)
-                model.note?.let { noteTxt -> tNote.setText(noteTxt) }
-                findViewById<Button>(R.id.btn_tran_delete)?.apply {
+                title = "Edit Transaction"
+                tfAmount.setText(model.money.toString())
+                tfName.setText(model.title)
+                tfDate.setText(model.date)
+                model.note?.let { noteTxt -> tfNote.setText(noteTxt) }
+                btnDelete.apply {
                     visibility = View.VISIBLE
                     setOnClickListener {
                         launch {
                             if (repository.delete(model.id)) setResult(RESULT_OK) else setResult(0)
-                            calculateDeleteMoney(tranType,model.money)
+                            calculateDeleteMoney(tranType, model.money)
                             finish()
                         }
                     }
@@ -70,7 +85,7 @@ class TransactionInputActivity : AppCompatActivity(), CoroutineScope {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
-        findViewById<StickySwitch>(R.id.sw_tran).apply {
+        swTran.apply {
             setDirection(
                 when (tranType) {
                     TransactionType.ADD -> StickySwitch.Direction.RIGHT
@@ -86,13 +101,17 @@ class TransactionInputActivity : AppCompatActivity(), CoroutineScope {
                 }
             })
         }
-        findViewById<Button>(R.id.btn_tran_save).setOnClickListener {
+        btnSave.setOnClickListener {
+            isAllFieldValidate = validateTransaction()
+            if (!isAllFieldValidate) {
+                return@setOnClickListener
+            }
             launch {
                 val model = TransactionModel(
-                    money = tAmount.text.toString().toFloat(),
-                    title = tTitle.text.toString(),
-                    date = tDate.text.toString(),
-                    note = tNote.text.let { if (it.isNullOrBlank()) null else it.toString() },
+                    money = tfAmount.text.toString().toFloat(),
+                    title = tfName.text.toString(),
+                    date = tfDate.text.toString(),
+                    note = tfNote.text.let { if (it.isNullOrBlank()) null else it.toString() },
                     type = tranType
                 )
                 Log.d("Debug", model.toString())
@@ -100,14 +119,14 @@ class TransactionInputActivity : AppCompatActivity(), CoroutineScope {
                     if (initData != null) {
                         //Edit transaction
                         repository.update(model)
-                        calculateEditMoney(tranType,initData.money, model.money)
+                        calculateEditMoney(tranType, initData.money, model.money)
                     } else {
                         //Add new transaction
                         repository.insert(model)
-                        calculateAddMoney(tranType,model.money)
+                        calculateAddMoney(tranType, model.money)
 
                     }
-                } catch (e:Exception){
+                } catch (e: Exception) {
                     Log.d("DEBUG", "Error save button \n $e")
                     setResult(0)
                     finish()
@@ -119,53 +138,83 @@ class TransactionInputActivity : AppCompatActivity(), CoroutineScope {
         }
     }
 
-    private fun calculateAddMoney(type: TransactionType, money: Float){
-        when(type){
+    private fun validateTransaction(): Boolean {
+        var hasBeforeFocus = false
+        val amountValid  = findViewById<TextFieldBoxes>(R.id.tfb_tran_amount).let {
+            if(it == null) return@let true
+            if (tfAmount.text.isEmpty()) {
+                it.setError("Please fill amount.", true)
+            }
+            !it.isOnError
+        }.also { hasBeforeFocus = it }
+        val nameValid = findViewById<TextFieldBoxes>(R.id.tfb_tran_title).let {
+            box ->
+            if(box == null ) return@let true
+            tfName.let {
+                if (it.text.isEmpty()) box.setError("Please fill transaction name.", hasBeforeFocus)
+                else if (it.text.length < 3) box.setError("Transaction name at least 3 characters.", true)
+                else if (it.text.length > 255) box.setError("Transaction name maximum 255 characters.", true)
+            }
+            !box.isOnError
+        }
+        Log.d("DEBUG", "Form valid: $amountValid - $nameValid")
+        return amountValid && nameValid
+    }
+
+    private fun calculateAddMoney(type: TransactionType, money: Float) {
+        when (type) {
             TransactionType.EXPENSE -> {
                 SharePrefHelper.create(this)?.get(SharePrefKey.MONEY_EXPENSE, 0f)?.also {
-                    SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_EXPENSE, (it + money) )
+                    SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_EXPENSE, (it + money))
                 }
             }
+
             TransactionType.ADD -> {
                 SharePrefHelper.create(this)?.get(SharePrefKey.MONEY_ADD, 0f)?.also {
                     SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_ADD, (it + money))
                 }
             }
-            TransactionType.UNKNOWN -> {
-                Toast.makeText(this, "Fail to calculate money! - Unknown type", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    private fun calculateDeleteMoney(type: TransactionType, money: Float){
-        when(type){
-            TransactionType.EXPENSE -> {
-                SharePrefHelper.create(this)?.get(SharePrefKey.MONEY_EXPENSE, 0f)?.also {
-                    SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_EXPENSE, (it - money) )
-                }
-            }
-            TransactionType.ADD -> {
-                SharePrefHelper.create(this)?.get(SharePrefKey.MONEY_ADD, 0f)?.also {
-                    SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_ADD, (it - money))
-                }
-            }
+
             TransactionType.UNKNOWN -> {
                 Toast.makeText(this, "Fail to calculate money! - Unknown type", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun calculateEditMoney(type: TransactionType,oldMoney: Float, money: Float){
-        when(type){
+    private fun calculateDeleteMoney(type: TransactionType, money: Float) {
+        when (type) {
             TransactionType.EXPENSE -> {
                 SharePrefHelper.create(this)?.get(SharePrefKey.MONEY_EXPENSE, 0f)?.also {
-                    SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_EXPENSE, (it + money - oldMoney ) )
+                    SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_EXPENSE, (it - money))
                 }
             }
+
+            TransactionType.ADD -> {
+                SharePrefHelper.create(this)?.get(SharePrefKey.MONEY_ADD, 0f)?.also {
+                    SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_ADD, (it - money))
+                }
+            }
+
+            TransactionType.UNKNOWN -> {
+                Toast.makeText(this, "Fail to calculate money! - Unknown type", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun calculateEditMoney(type: TransactionType, oldMoney: Float, money: Float) {
+        when (type) {
+            TransactionType.EXPENSE -> {
+                SharePrefHelper.create(this)?.get(SharePrefKey.MONEY_EXPENSE, 0f)?.also {
+                    SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_EXPENSE, (it + money - oldMoney))
+                }
+            }
+
             TransactionType.ADD -> {
                 SharePrefHelper.create(this)?.get(SharePrefKey.MONEY_ADD, 0f)?.also {
                     SharePrefHelper.create(this)?.put(SharePrefKey.MONEY_ADD, (it + money - oldMoney))
                 }
             }
+
             TransactionType.UNKNOWN -> {
                 Toast.makeText(this, "Fail to calculate money! - Unknown type", Toast.LENGTH_SHORT).show()
             }
